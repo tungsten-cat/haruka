969ef99afbd48aa854733e8a7bcef94707f96ff1
tungsten-cat <uly4nkin.denis@gmail.com>
2023-04-14 22:52:30 +0300

    Added handler for working with JSON files

---
+++ sources/kernel/handlers/json.py
@@ -0,0 +1,15 @@
+
+# Importing module for working with JSON files
+import json
+
+def json_reader(json_file):
+    # Define variable that will contain this JSON file content
+    json_content = 0
+
+    # Here we'll open our file for reading
+    with open(json_file, "r") as json_handler:
+        # And store all contents to pre-defined variable
+        json_content = json.load(json_handler)
+
+    # Now we should return JSON file content
+    return json_content
