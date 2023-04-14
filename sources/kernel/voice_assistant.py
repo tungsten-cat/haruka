3b2daa218a4760f9b97d45a531d8ea774c5fa718
tungsten-cat <uly4nkin.denis@gmail.com>
2023-04-14 23:32:05 +0300

    Created functions for assistant

--- sources/kernel/voice_assistant.py
+++ sources/kernel/voice_assistant.py
@@ -1,7 +1,10 @@
 
-import modules.stt_system as stt_system
-import modules.tts_system as tts_system
-
 class VoiceAssistant:
     def __init__(self):
+        pass
+
+    def setup_modules(self, config_file):
+        pass
+
+    def setup_handlers(self):
         pass
