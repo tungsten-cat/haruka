a7203d514833e97d4f8ab1bdad172356e30e4872
tungsten-cat <uly4nkin.denis@gmail.com>
2023-04-15 13:29:18 +0300

    Initialised configuration files pointers; Created assistant object

--- sources/main.py
+++ sources/main.py
@@ -1,5 +1,14 @@
 
-import kernel.voice_assistant as voice_assistant
+import kernel.voice_assistant
+
+VOICE_ASSISTANT = kernel.voice_assistant.VoiceAssistant()
+
+import os
+
+CURRENT_DIR = os.getcwd()
+
+VA_MODULES_LIST = os.path.join(CURRENT_DIR, "sources", "modules.json")
+VA_COMMANDS_LIST = os.path.join(CURRENT_DIR, "sources", "commands.json")
 
 if __name__ == "__main__":
-    pass
+    VOICE_ASSISTANT.setup_modules(VA_MODULES_LIST)
