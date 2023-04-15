88c3bb018fa5c457458f7833dd427e3ecd5f8a9c
tungsten-cat <uly4nkin.denis@gmail.com>
2023-04-15 13:46:48 +0300

    Code review with adding several comments

--- sources/kernel/handlers/importer.py
+++ sources/kernel/handlers/importer.py
@@ -1,8 +1,25 @@
 
+# Importing module for handling another modules import
 import importlib
+
+# Importing module for working with OS API
+import os
+# Importing module for working with ENV files
+import dotenv
+
+# Looking for ENV configuration file
+dotenv_config = dotenv.find_dotenv()
+# Loading variables from ENV configuration file
+dotenv.load_dotenv(dotenv_config)
+
+# Setting variable for configuring PROJECT_MODE (i.e. DEBUG or RELEASE)
+PROJECT_CONFIG = os.environ.get("PROJECT_CONFIG")
 
 def import_module(module_name, package_name = None):
     try:
         module = importlib.import_module(module_name, package_name)
     except ImportError:
         print(f"Error: unable to import module {module_name}!")
+    else:
+        if PROJECT_CONFIG == "DEBUG":
+            print(f"Successfully imported module {module_name}!")
--- sources/main.py
+++ sources/main.py
@@ -3,12 +3,17 @@ import kernel.voice_assistant
 
 VOICE_ASSISTANT = kernel.voice_assistant.VoiceAssistant()
 
+# Importing module for working with OS API
 import os
+# Importing module for working with ENV files
 import dotenv
 
+# Looking for ENV configuration file
 dotenv_config = dotenv.find_dotenv()
+# Loading variables from ENV configuration file
 dotenv.load_dotenv(dotenv_config)
 
+# Setting variable for configuring PROJECT_MODE (i.e. DEBUG or RELEASE)
 PROJECT_CONFIG = os.environ.get("PROJECT_CONFIG")
 
 CURRENT_DIR = os.getcwd()
