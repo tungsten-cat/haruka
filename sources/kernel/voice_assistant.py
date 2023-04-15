c6dca460477c19b02cb8428556eea96ae10af2bf
tungsten-cat <uly4nkin.denis@gmail.com>
2023-04-15 13:29:39 +0300

    Created function for setting up commands

--- sources/kernel/voice_assistant.py
+++ sources/kernel/voice_assistant.py
@@ -1,13 +1,16 @@
 
-import handlers.json
-import handlers.importer
+import kernel.handlers.json as json_handler
+import kernel.handlers.importer as import_handler
 
 class VoiceAssistant:
     def __init__(self):
         pass
 
     def setup_modules(self, modules_file):
-        modules_list = handlers.json.get_file_content(modules_file)
+        modules_list = json_handler.get_file_content(modules_file)
 
         for module in modules_list:
-            handlers.importer.import_module(modules_list[module])
+            import_handler.import_module(modules_list[module])
+
+    def setup_commands(self, commands_file):
+        pass
